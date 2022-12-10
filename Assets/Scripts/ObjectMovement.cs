a143da182623b1317fd340b95bd807b026ff6b97
Ahmet Eren Kardaş <erenkardas@gmail.com>
2022-12-10 21:59:27 +0300

    start

--- Assets/Scripts/ObjectMovement.cs
+++ Assets/Scripts/ObjectMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Meteor : MonoBehaviour
+public class ObjectMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
